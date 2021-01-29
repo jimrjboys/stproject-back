db35a3a80a72ad9d5cf1b79f2fa334e28f906b3b
jimrjboys <jimrjboys@gmail.com>
2021-01-29 15:23:45 +0300

    ok pour les notifications

--- src/models/Localisation.js
+++ src/models/Localisation.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema ;
 
 export const  LocalisationSchema  = new Schema ({
-    timestamps : true , 
+ 
     longitude : Number , 
     latitude : Number ,
     idUtilisateur : String , 
-})
+    
+},{timestamps : true , })
--- src/routes/STroutes.js
+++ src/routes/STroutes.js
@@ -1,14 +1,33 @@
-import { listUtilisateur,ajouterUtilisateur,utilisateurId, modifierUtilisateur} from '../controllers/UtilisateurController'
-
+import {    listUtilisateur,
+    ajouterUtilisateur,
+    utilisateurId, 
+    modifierUtilisateur,
+    SaveLastLocalisation ,
+    softDelete   } from '../controllers/UtilisateurController'
+import {ajoutNotification ,modificationNotification } from '../controllers/NotificationControllers'
 const route = (app) => {
     // Utilisateur
     app.route ('/utilisateur')
-    .get(listUtilisateur)
-    .post(ajouterUtilisateur) 
+        .get(listUtilisateur)
+        .post(ajouterUtilisateur) 
     
     //Utilisateur avec Recheche par ID 
     app.route('/utilisateur/:utilisateurId')
         .get(utilisateurId)
         .put(modifierUtilisateur)
+        
+    //sauvegrade du derniere position  de l'utilisateur
+    app.route('/localisationActuelle')
+        .post(SaveLastLocalisation)
+    
+    //supprimer compte utilisateur 
+    app.route('/supprime/utilisateur/:utilisateurId')
+    .put(softDelete)
+
+    //Notification ajout et modification
+    app.route('/notification/')
+    .post(ajoutNotification)
+    app.route('/notification/:notificationId')
+    .put(modificationNotification)
 }
 export default route;
