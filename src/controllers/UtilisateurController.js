4c81b4320fe45ae801571df96b74d9aa33e76265
jimrjboys <jimrjboys@gmail.com>
2021-04-04 21:09:31 +0300

    quelque modif finish login

--- src/controllers/UtilisateurController.js
+++ src/controllers/UtilisateurController.js
@@ -159,7 +159,7 @@ export const Authentification = (req, res) => {
         if (!utilisateur || !utilisateur.comparePassword(req.body.password)) {
             return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
         }
-        return res.json({ token: jwt.sign({ email: utilisateur.email, username: utilisateur.username, _id: utilisateur._id, mailVerify: utilisateur.mailVerify, expiresIn: 31556926  , success : true}, 'RESTFULAPIs') });
+        return res.json({ token: jwt.sign({ email: utilisateur.email, username: utilisateur.username, _id: utilisateur._id, mailVerify: utilisateur.mailVerify, expiresIn: 31556926, success : true}, 'RESTFULAPIs') });
     });
 };
 export const VerificationAuthentification = (req, res, next) => {
