96295290a2b610a07fad034008662fb954abbc40
shiroe1036 <shiroeeric@gmail.com>
2021-06-25 17:55:19 +0300

    MAJ

--- index.js
+++ index.js
@@ -57,12 +57,4 @@ app.get('/', (req, res) =>
     res.send(`notre serveur a été demarer sur le port : ${process.env.PORT || 8080}`)
 );
 
-app.listen(process.env.PORT || 8080, () =>{
-  const dir = './upload'
-  if(!fs.existsSync(dir)){
-      fs.mkdirSync(dir, { recursive: true }, err => console.log(err))
-  }
-  // console.log(`Notre serveur est en marche dans le port ${process.env.PORT || 3000}`)
-  console.log(`Notre serveur est en marche dans le port ${process.env.PORT || 8080}`)
-});
-// export default app ; 
+export default app ; 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// import app from './index'
-// import fs from "fs"
+import app from './index'
+import fs from "fs"
 
-// app.listen(process.env.PORT || 8080, () =>{
-//     const dir = './upload'
-//     if(!fs.existsSync(dir)){
-//         fs.mkdirSync(dir, { recursive: true }, err => console.log(err))
-//     }
-//     // console.log(`Notre serveur est en marche dans le port ${process.env.PORT || 3000}`)
-//     console.log(`Notre serveur est en marche dans le port ${process.env.PORT || 8080}`)
-// });
+app.listen(process.env.PORT || 8080, () =>{
+    const dir = './upload'
+    if(!fs.existsSync(dir)){
+        fs.mkdirSync(dir, { recursive: true }, err => console.log(err))
+    }
+    // console.log(`Notre serveur est en marche dans le port ${process.env.PORT || 3000}`)
+    console.log(`Notre serveur est en marche dans le port ${process.env.PORT || 8080}`)
+});
